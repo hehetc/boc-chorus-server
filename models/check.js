0ad20cea7b5cc94aa5d6a781420aa52a8c8ddb3e
hehetc <chyin2010@hotmail.com>
2018-12-05 19:23:38 +0800

    small change

--- models/check.js
+++ models/check.js
@@ -2,7 +2,7 @@ var db = require('../utils/dbConnection');
 var moment = require('moment');
 module.exports = {
     check: function (user_id,activity_id,photo_url,callback) {
-        var sql = 'INSERT INTO T_check(F_ID,F_ActivityId,F_UserID,F_CheckDate,F_PhotoUrl)VALUES(0,?,?,?,?)';
+        var sql = 'INSERT INTO t_check(F_ID,F_ActivityId,F_UserID,F_CheckDate,F_PhotoUrl)VALUES(0,?,?,?,?)';
         var now = new Date();
         var params = [activity_id, user_id, now, photo_url];
         console.log(params);
